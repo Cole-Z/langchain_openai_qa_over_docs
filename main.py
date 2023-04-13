40ade6bc02f663a0dbb1c438f9694362c1ba5fc8
Cole Zill <98670265+Cole-Z@users.noreply.github.com>
2023-04-12 22:08:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,41 +8,21 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
 
-# this just ensures the response being generated is completed. 
-def truncate_to_last_sentence(text):
-    end_punctuations = [".", "!", "?"]
-    for i in range(len(text) - 1, -1, -1):
-        if text[i] in end_punctuations:
-            return text[:i+1]
-    return text
-
-# Refine answer function
-def refine_answer(prompt, model_name="text-davinci-002", temperature=0.5, max_tokens=100):
-    response = openai.Completion.create(
-        engine=model_name,
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        n=1,
-        stop=['Human:', 'AI:'],
-        echo=False,
+def chat_gpt_response(conversation, model_name="gpt-3.5-turbo", max_response_tokens=250):
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=conversation,
+        temperature=0.7,
+        max_tokens=max_response_tokens,
     )
+    return response['choices'][0]['message']['content'].strip()
 
-    full_response = response.choices[0].text.strip()
-    truncated_response = truncate_to_last_sentence(full_response)
-    return truncated_response
+def ask_question(conversation, question):
+    conversation.append({"role": "user", "content": question})
+    response = chat_gpt_response(conversation, model_name="gpt-3.5-turbo")
+    conversation.append({"role": "assistant", "content": response})
+    return response
 
-# Ask question function
-def ask_question(qa_instance, question, refine=False):
-    initial_answer = qa_instance.run(question)
-    if refine:
-        prompt = f"You are a knowledgeable assistant. Please provide a more detailed and a refined answer to the question: {question}\n\nInitial answer: {initial_answer}\n\nRefined answer:"
-        refined_answer = refine_answer(prompt)
-        return refined_answer
-
-    return initial_answer
-
-# Read the contents of the 'kb.txt' file into the zlg_info variable with UTF-8 encoding
 with open('kb.txt', 'r', encoding='utf-8') as file:
     kb_info = file.read()
 
@@ -54,11 +34,23 @@ vectorstore = FAISS.from_texts(texts, embeddings)
 
 qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=vectorstore)
 
-# Bot loop
+conversation = [
+    {
+        "role": "system",
+        "content": "You are a helpful assitant with expertise in Linux and data bases."
+    }
+]
+
+max_conversation_length = 5
+
 while True:
     user_question = input("Ask a question or type 'exit' to quit: ")
     if user_question.lower() == "exit":
         break
 
-    refined_answer = ask_question(qa, user_question, refine=True)
-    print("Bot:", refined_answer)
+    if len(conversation) > max_conversation_length * 2 - 1:  # Multiply by 2 to account for user and assistant messages
+        conversation.pop(0)
+        conversation.pop(0)
+
+    response = ask_question(conversation, user_question)
+    print("Bot:", response)
